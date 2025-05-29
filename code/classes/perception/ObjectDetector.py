a402f1c12da2c164fc6402dacfb46dba90dc91fb
Nick Struckmeyer <struckmeyerni@gmail.com>
2025-05-29 15:13:31 +0200

    Switched to new YOLOE model

--- code/classes/perception/ObjectDetector.py
+++ code/classes/perception/ObjectDetector.py
@@ -1,18 +1,26 @@
-from ultralytics import YOLO
+from ultralytics import YOLOE
+import logging
+
+# Necessary in oder to prevent yolo from spamming the console
+logging.getLogger("ultralytics").setLevel(logging.ERROR)
 
 class ObjectDetector:
     def __init__(self, model_path: str):
         self._model = self.create_custom_yolo_model(model_path)
 
     def create_custom_yolo_model(self, model_path: str):
-        model = YOLO(f"{model_path}/yolov8s-worldv2.pt")
-        model.set_classes(
-            ["table", "monitor", "closed door", "open door", "chair", "computer",
+        model = YOLOE(f"{model_path}/yoloe-11l-seg.pt")
+
+        classes = ["table", "monitor", "closed door", "open door", "chair", "computer",
              "person", "fridge", "fire extinguisher", "window", "blackboard",
              "kitchen cabinet", "wall", "toilet", "towel", "radiator", "desk",
              "bin"
              ]
+
+        model.set_classes(
+            classes, model.get_text_pe(classes)
         )
+        
         model.save(f"{model_path}/yolo_model_custom.pt")
 
         return model
